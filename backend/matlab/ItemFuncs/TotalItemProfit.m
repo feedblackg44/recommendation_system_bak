275332dd39dc64c935fe443800758616ed77a0af
feedblackg44 <pilmyh@gmail.com>
2024-05-19 22:35:37 +0300

    removed extra files

--- backend/matlab/ItemFuncs/TotalItemProfit.m
+++
@@ -1,5 +0,0 @@
-function total_item_profit = TotalItemProfit(item, memoBSBD)
-profit = Profit(item, 0, memoBSBD);
-sold_amount = SoldAmount(item);
-total_item_profit = profit * sold_amount;
-end
--- backend/matlab/Tools/WriteOutTable.m
+++
@@ -1,9 +0,0 @@
-function WriteOutTable(table_out, table_in, second_table, third_table, out_name)
-    if exist(out_name, 'file') == 2
-        delete(out_name)
-    end
-    writetable(table_out, out_name, 'Sheet', 'Sheet1');
-    writetable(table_in, out_name, 'Sheet', 'Sheet2');
-    writetable(second_table, out_name, 'Sheet', 'Sheet3');
-    writetable(third_table, out_name, 'Sheet', 'Sheet4');
-end
